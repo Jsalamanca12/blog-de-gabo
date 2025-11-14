d9608b05eb58e57fc32bf6fea161a5cad1b4d5a4
Jhon Jairo Salamanca Pacheco <jsalamanca@informatica.local>
2025-11-14 15:09:50 -0500

    Actualizo meta tag de verificación de Google

--- app.py
+++ app.py
@@ -132,5 +132,8 @@ def robots():
     return send_from_directory('static', 'robots.txt')
 
 
+#if __name__ == '__main__':
+   # app.run(host='0.0.0.0', port=5000)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
